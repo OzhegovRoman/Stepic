faea9ad8fbf378a2ee372c204f8d84e0b4173300
Stepic <Ozhegov_Roman@mail.ru>
2016-02-09 10:42:54 +0000

    test

---
+++ L6.3/tes1.c
@@ -0,0 +1,11 @@
+#include <sys/types.h> 
+#include <sys/ipc.h>
+#include <sys/sem.h>
+union semun { int val; unsigned short *array; };
+int main() {
+key_t key = ftok("/tmp/sem.temp", 1);
+int id = semget(key, 16, 0660);
+union semun a; unsigned short v[16]; a.array = v;
+semctl(id, 0, GETALL, a);
+unsigned int i; for(i = 0; i < 16; i++) if(v[i] != i) return 1;
+return 0; }

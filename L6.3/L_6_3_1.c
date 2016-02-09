87144724b85fc34d3733bf92cd3956d970b6646c
Roman <Ozhegov_Roman@mail.ru>
2016-02-09 13:38:26 +0300

    L_6.3.1

---
+++ L6.3/L_6_3_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/types.h>
+#include <linux/sem.h>
+#include <linux/ipc.h>
+#include <unistd.h>
+
+int main()
+{
+    key_t key = ftok("/tmp/sem.temp, 1");
+    int semid = semget(key, 16, 0660 | IPC_CREAT);
+    int i;
+
+    union semun arg;
+    struct sembuf sem_opt[16];
+    for (i = 0; i<16; i++){
+        arg.val = i;
+        semctl(semid, i, SETVAL, arg);
+    }
+
+    for (i = 0; i< 16; i++){
+        int semval = semctl(semid, i, GETVAL, 0);
+        printf("%d:%d\n",i,semval);
+    }
+
+    pause();
+}
